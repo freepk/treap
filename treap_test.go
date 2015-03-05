372691db93b90782d33e86e1dbe8308b36e35172
freepk <freepk@mail.ru>
2015-03-06 02:23:34 +0300

    Create treap_test.go

---
+++ treap_test.go
@@ -0,0 +1,30 @@
+package treap
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestTreap(t *testing.T) {
+	x := newTreap(4, 6, nil, nil)
+	x = x.add(6, 2)
+	x = x.add(13, 8)
+	x = x.add(7, 10)
+	x = x.add(11, 3)
+
+	if x.x != 7 {
+		t.Fail()
+	}
+
+	for i := 1; i < 10000000; i++ {
+		x = x.add(uint32(i), rand.Uint32())
+	}
+	//traverse(x)
+}
+
+func BenchmarkTreap(b *testing.B) {
+	x := newTreap(0, rand.Uint32(), nil, nil)
+	for i := 1; i <= b.N; i++ {
+		x = x.add(uint32(i), rand.Uint32())
+	}
+}
